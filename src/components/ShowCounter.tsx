773f8a7b7566e17ee31c75235489703bb307c311
Justin Chiou <chiou.kai@gmail.com>
2022-04-15 08:52:51 -0400

    fix: remove link

--- src/components/ShowCounter.tsx
+++ src/components/ShowCounter.tsx
@@ -10,12 +10,7 @@ interface Props {
 const ShowCounter = ({ days, hours, minutes, seconds }: Props) => {
   return (
     <div className="show-counter">
-      <a
-        href="https://tapasadhikary.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="countdown-link"
-      >
+      <div className="countdown-link">
         <DateTimeDisplay value={days} type={"Days"} isDanger={days <= 3} />
         <p>:</p>
         <DateTimeDisplay value={hours} type={"Hours"} isDanger={false} />
@@ -23,7 +18,7 @@ const ShowCounter = ({ days, hours, minutes, seconds }: Props) => {
         <DateTimeDisplay value={minutes} type={"Mins"} isDanger={false} />
         <p>:</p>
         <DateTimeDisplay value={seconds} type={"Seconds"} isDanger={false} />
-      </a>
+      </div>
     </div>
   );
 };
